2d9db119ef5e4164a572ef4707f2c174e1a1f1e8
ccantero <ccantero>
2018-09-11 01:39:22 -0300

    Update structure in Client

--- UserClient/src/main/java/com/showtime/rest/UserClient/Tester.java
+++ UserClient/src/main/java/com/showtime/rest/UserClient/Tester.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.UriBuilder;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.filter.LoggingFilter;
 
-import com.showtime.rest.User;
+import main.java.com.showtime.rest.User;
 
 
 public class Tester {
