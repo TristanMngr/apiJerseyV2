6aa3596061d7f1ed8651f97554f842221ef742d3
ccantero <cantero.cristhian@gmail.com>
2018-09-18 08:43:03 -0300

    Add new URL

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,13 +1,8 @@
 package controller;
 
-import java.io.IOException;
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -42,49 +37,3 @@ public class Main {
     }
 
 }
-//
-//
-//public class JettyFilter {
-//
-//	  public static void main(final String[] args) throws Exception {
-//	    Server server = new Server(8080);
-//
-//	    ServletHandler handler = new ServletHandler();
-//	    server.setHandler(handler);
-//
-//	    handler.addServletWithMapping(HelloServlet.class, "/*");
-//	    handler.addFilterWithMapping(HelloPrintingFilter.class, "/*",
-//	        EnumSet.of(DispatcherType.REQUEST));
-//
-//	    server.start();
-//	    server.join();
-//	  }
-//
-//	  public static class HelloServlet extends HttpServlet {
-//	    private static final long serialVersionUID = 1L;
-//
-//	    @Override
-//	    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//	        throws ServletException, IOException {
-//	      response.setContentType("text/html");
-//	      response.setStatus(HttpServletResponse.SC_OK);
-//	      response.getWriter().println("<h1>Hello SimpleServlet</h1>");
-//	    }
-//	  }
-//
-//	  public static class HelloPrintingFilter implements Filter {
-//	    @Override
-//	    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//	        throws IOException, ServletException {
-//	      System.out.print("hello from filter");
-//	    }
-//
-//	    @Override
-//	    public void init(FilterConfig arg0) throws ServletException {
-//
-//	    }
-//
-//	    @Override
-//	    public void destroy() {}
-//	  }
-//	}
