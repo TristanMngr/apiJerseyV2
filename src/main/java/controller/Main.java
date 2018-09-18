a9c842976aa69f685f463b24fb269e30dc88c9cd
ccantero <cantero.cristhian@gmail.com>
2018-09-18 08:53:17 -0300

    Change in URL

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -2,36 +2,39 @@ package controller;
 
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
-import org.glassfish.jersey.servlet.ServletContainer;
 
 /**
  * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
  * command that is used for launching should fire this main method.
  */
 public class Main {
-	
-    public static void main(String[] args) throws Exception {
-    
-    	ResourceConfig config = new ResourceConfig();
-        config.packages("controller");
-        config.property(JspMvcFeature.TEMPLATES_BASE_PATH, "/WEB-INF/jsp");
-        config.register(JspMvcFeature.class);
-        
-        ServletContainer servletContainer = new ServletContainer(config);
-        ServletHolder jerseyServlet = new ServletHolder(servletContainer);
+   
+    public static void main(String[] args) throws Exception{
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
 
+        final Server server = new Server(Integer.valueOf(webPort));
         final WebAppContext root = new WebAppContext();
-        
-        Server server = new Server(8080);
-        ServletContextHandler context = new ServletContextHandler(server, "/");
-        //context.addFilter(jerseyServlet, "/*",EnumSet.of(DispatcherType.REQUEST));
-        //context.addFilter(null, "/*", EnumSet.of(DispatcherType.REQUEST));
-        context.addServlet(jerseyServlet, "/*");
+
+        root.setContextPath("/");
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
+
+        final String webappDirLocation = "src/main/webapp/";
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
+        server.setHandler(root);
+
         server.start();
         server.join();
     }
