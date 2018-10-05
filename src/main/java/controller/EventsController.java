e773a933e6a074a542773414dc151fecbe9ab6c8
Guille <gafernandez2525@gmail.com>
2018-10-05 15:41:05 -0300

    primera versión de entorno web bootstrap

--- src/main/java/controller/EventsController.java
+++ src/main/java/controller/EventsController.java
@@ -34,266 +34,266 @@ import service.EventsService;
 @Path("/events")
 public class EventsController {
 
-	static final long idEventBrite = 17920884849L;
+    static final long idEventBrite = 17920884849L;
 
-	private EventsService eventsService = new EventsService();
-	private EventBrite eventBrite = new EventBrite();
-	final Gson gson = new Gson();
+    private EventsService eventsService = new EventsService();
+    private EventBrite eventBrite = new EventBrite();
+    final Gson gson = new Gson();
 
-	public static String formatString(String text) {
+    public static String formatString(String text) {
 
-		StringBuilder json = new StringBuilder();
-		String indentString = "";
+        StringBuilder json = new StringBuilder();
+        String indentString = "";
 
-		for (int i = 0; i < text.length(); i++) {
-			char letter = text.charAt(i);
-			switch (letter) {
-			case '{':
-			case '[':
-				json.append("\n" + indentString + letter + "\n");
-				indentString = indentString + "\t";
-				json.append(indentString);
-				break;
-			case '}':
-			case ']':
-				indentString = indentString.replaceFirst("\t", "");
-				json.append("\n" + indentString + letter);
-				break;
-			case ',':
-				json.append(letter + "\n" + indentString);
-				break;
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+            switch (letter) {
+                case '{':
+                case '[':
+                    json.append("\n" + indentString + letter + "\n");
+                    indentString = indentString + "\t";
+                    json.append(indentString);
+                    break;
+                case '}':
+                case ']':
+                    indentString = indentString.replaceFirst("\t", "");
+                    json.append("\n" + indentString + letter);
+                    break;
+                case ',':
+                    json.append(letter + "\n" + indentString);
+                    break;
 
-			default:
-				json.append(letter);
-				break;
-			}
-		}
+                default:
+                    json.append(letter);
+                    break;
+            }
+        }
 
-		return json.toString();
-	}
+        return json.toString();
+    }
 
-	public static List<Event> MaestroEventos = new ArrayList<Event>();
-	public static List<EventsList> MaestroListasEventos = new ArrayList<EventsList>();
+    public static List<Event> MaestroEventos = new ArrayList<Event>();
+    public static List<EventsList> MaestroListasEventos = new ArrayList<EventsList>();
 
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response get() {
-		return Response.status(201).entity(this.eventsService.allEvents()).build();
-	}
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public Response get() {
+//        return Response.status(201).entity(this.eventsService.allEvents()).build();
+//    }
+    @GET
+    public Viewable mainEventos() {
+        return new Viewable("/jsp/eventos/index");
+    }
 
-	/**
-	 * buscar un evento por ID,
-	 * 
-	 * @param params
-	 * @return
-	 */
-	@Path("/buscarEvento")
-	@POST
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	@Produces({ MediaType.TEXT_HTML, MediaType.APPLICATION_JSON })
-	public Response buscarEvento(String param) {
-	//public Viewable buscarEvento(String param) {
-		Long id = 0L;
-		String searchPattern = "";
-		Map<String, String> model = new HashMap<String, String>();
-		model.put("codigo", "");
-		model.put("nombre", "");
-		model.put("fecha", "");
-		model.put("hora", "");
+    /**
+     * buscar un evento por ID,
+     *
+     * @param params
+     * @return
+     */
+    @Path("/buscarEvento")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON})
+    public Response buscarEvento(String param) {
+        //public Viewable buscarEvento(String param) {
+        Long id = 0L;
+        String searchPattern = "";
+        Map<String, String> model = new HashMap<String, String>();
+        model.put("codigo", "");
+        model.put("nombre", "");
+        model.put("fecha", "");
+        model.put("hora", "");
 
-		List<String> parametros = Arrays.asList(param.split("=", -1));
-		
-		if(parametros.get(1).matches("\\d+"))
-			id = Long.valueOf(parametros.get(1));
-		else
-			searchPattern = parametros.get(1);
-		
-		Date date = new Date();
-		
-		// TODO: Cristhian: Solo por testing.
-		if (id == idEventBrite) {
-			eventBrite = gson.fromJson(EventbriteApi.getEventByID(id.toString()), EventBrite.class);
-			model.put("codigo", eventBrite.getId().toString());
-			model.put("nombre", eventBrite.getName().getText());
-						
-			try {
-				DateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-				date = iso8601.parse(eventBrite.getStart().getLocal());
-				
-				SimpleDateFormat simpleDateFormat_Date = new SimpleDateFormat("yyyy-MM-dd");
-				String fechaEvento = simpleDateFormat_Date.format(date);
-				
-				SimpleDateFormat simpleDateFormat_Time = new SimpleDateFormat("HH:mm:ss");
-				String horaEvento = simpleDateFormat_Time.format(date);
-				model.put("fecha", fechaEvento);
-				model.put("hora", horaEvento);
-				
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
+        List<String> parametros = Arrays.asList(param.split("=", -1));
 
-		}
-		else if ( !searchPattern.equals(""))
-		{
-			// TODO: No pude hacer andar el Viewable con un listado.
-			String results = EventbriteApi.getEventByName(searchPattern);
-			List<Event> listado = eventsService.getFromPagination(results);
-			//return new Viewable("/jsp/test", model);
-			return Response.status(201).entity(listado).build();
-		}
-		else {
-			Event evento = this.eventsService.getEventById(id);
-			if (evento != null) {
-				model.put("codigo", evento.getId().toString());
-				model.put("nombre", evento.getNombre());
-				model.put("fecha", evento.getFecha());
-				model.put("hora", evento.getHora());
-			}
-			
-		}
-		//return new Viewable("/jsp/eventos/evento", model);
-		return Response.ok(new Viewable("/jsp/eventos/evento", model)).build();
-	}
+        if (parametros.get(1).matches("\\d+")) {
+            id = Long.valueOf(parametros.get(1));
+        } else {
+            searchPattern = parametros.get(1);
+        }
 
-	@Path("/cantidad")
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response getAmountEvents() {
-		return Response.status(201).entity("Hay " + MaestroEventos.size() + " eventos").build();
-	}
+        Date date = new Date();
 
-	@Path("/crearEvento/{nombre}/{fecha}/{hora}")
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response crearEvento(@PathParam("nombre") String nombre, @PathParam("fecha") String fecha,
-			@PathParam("hora") String hora) {
+        // TODO: Cristhian: Solo por testing.
+        if (id == idEventBrite) {
+            eventBrite = gson.fromJson(EventbriteApi.getEventByID(id.toString()), EventBrite.class);
+            model.put("codigo", eventBrite.getId().toString());
+            model.put("nombre", eventBrite.getName().getText());
 
-		Event nuevoEvento = new Event(nombre, fecha, hora);
-		MaestroEventos.add(nuevoEvento);
-		// System.out.println("Se creó el evento " + listaEventos.get(0).getFecha());
-		return Response.status(201).entity(MaestroEventos).build();
-	}
+            try {
+                DateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                date = iso8601.parse(eventBrite.getStart().getLocal());
 
-	/**
-	 * Busca eventos por varios parametros
-	 * 
-	 * @param nombre
-	 * @return lista de eventos
-	 */
-	@Path("/buscarEventos")
-	@POST
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	@Produces(MediaType.TEXT_HTML)
-	public Viewable buscarEventos(String params) {
-		Long id = 0L;
+                SimpleDateFormat simpleDateFormat_Date = new SimpleDateFormat("yyyy-MM-dd");
+                String fechaEvento = simpleDateFormat_Date.format(date);
+
+                SimpleDateFormat simpleDateFormat_Time = new SimpleDateFormat("HH:mm:ss");
+                String horaEvento = simpleDateFormat_Time.format(date);
+                model.put("fecha", fechaEvento);
+                model.put("hora", horaEvento);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        } else if (!searchPattern.equals("")) {
+            // TODO: No pude hacer andar el Viewable con un listado.
+            String results = EventbriteApi.getEventByName(searchPattern);
+            List<Event> listado = eventsService.getFromPagination(results);
+            //return new Viewable("/jsp/test", model);
+            return Response.status(201).entity(listado).build();
+        } else {
+            Event evento = this.eventsService.getEventById(id);
+            if (evento != null) {
+                model.put("codigo", evento.getId().toString());
+                model.put("nombre", evento.getNombre());
+                model.put("fecha", evento.getFecha());
+                model.put("hora", evento.getHora());
+            }
+
+        }
+        //return new Viewable("/jsp/eventos/evento", model);
+        return Response.ok(new Viewable("/jsp/eventos/evento", model)).build();
+    }
+
+    @Path("/cantidad")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getAmountEvents() {
+        return Response.status(201).entity("Hay " + MaestroEventos.size() + " eventos").build();
+    }
+
+    @Path("/crearEvento/{nombre}/{fecha}/{hora}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response crearEvento(@PathParam("nombre") String nombre, @PathParam("fecha") String fecha,
+            @PathParam("hora") String hora) {
+
+        Event nuevoEvento = new Event(nombre, fecha, hora);
+        MaestroEventos.add(nuevoEvento);
+        // System.out.println("Se creó el evento " + listaEventos.get(0).getFecha());
+        return Response.status(201).entity(MaestroEventos).build();
+    }
+
+    /**
+     * Busca eventos por varios parametros
+     *
+     * @param params
+     * @return lista de eventos
+     */
+    @Path("/buscarEventos")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
+    public Viewable buscarEventos(String params) {
+        Long id = 0L;
 //		String nombre = "";
-		Map<String, String> model = new HashMap<String, String>();
+        Map<String, String> model = new HashMap<String, String>();
 
-		List<String> parametros = Arrays.asList(params.split("&", -1));
+        List<String> parametros = Arrays.asList(params.split("&", -1));
 //		List<String> valores=parametros.stream().map(p->p.split("=",1)).collect(Collector.toList());
-		List<List<String>> valores = new ArrayList<List<String>>();
-		for (String p : parametros) {
-			valores.add(Arrays.asList(p.split("=", -1)));
-		}
-		id = Long.valueOf(valores.get(0).get(1));
+        List<List<String>> valores = new ArrayList<List<String>>();
+        for (String p : parametros) {
+            valores.add(Arrays.asList(p.split("=", -1)));
+        }
+        id = Long.valueOf(valores.get(0).get(1));
 //		nombre = valores.get(1).get(1);
 
-		if (id == idEventBrite) {
-			eventBrite = gson.fromJson(EventbriteApi.getEventByID(id.toString()), EventBrite.class);
-			System.out.println("algo del json" + eventBrite.getSubcategoryId());
-			return new Viewable("/index", model);
+        if (id == idEventBrite) {
+            eventBrite = gson.fromJson(EventbriteApi.getEventByID(id.toString()), EventBrite.class);
+            System.out.println("algo del json" + eventBrite.getSubcategoryId());
+            return new Viewable("/index", model);
 //			return Response.status(201).entity(formatString(EventbriteApi.getEvent(id.toString()))).build();
 //			model.put("nombre", "Hello");
 //			model.put("descripcion", "World! I'm index.jsp");
 //			model.put("start",);
 //			model.put("finish");
-		} else {
-			Event evento = this.eventsService.getEventById(id);
-			model.put("codigo", evento.getId().toString());
-			model.put("nombre", evento.getNombre());
-			model.put("fecha", evento.getFecha());
-			model.put("hora", evento.getHora());
-			return new Viewable("/eventos/evento", model);
-		}
-	}
+        } else {
+            Event evento = this.eventsService.getEventById(id);
+            model.put("codigo", evento.getId().toString());
+            model.put("nombre", evento.getNombre());
+            model.put("fecha", evento.getFecha());
+            model.put("hora", evento.getHora());
+            return new Viewable("/eventos/evento", model);
+        }
+    }
 
-	/**
-	 * 
-	 * @param nombre nombre de la lista de eventos
-	 * @return imprime en pantalla el json de lassss listassss de eventos
-	 */
-	@Path("/crearListaEventos/{nombreLista}")
-	@POST
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response crearListaEventos(@PathParam("nombreLista") String nombre) {
+    /**
+     *
+     * @param nombre nombre de la lista de eventos
+     * @return imprime en pantalla el json de lassss listassss de eventos
+     */
+    @Path("/crearListaEventos/{nombreLista}")
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response crearListaEventos(@PathParam("nombreLista") String nombre) {
 
-		MaestroListasEventos.add(new EventsList(nombre));
-		// System.out.println("Se creó el evento " + listaEventos.get(0).getFecha());
-		return Response.status(201).entity(MaestroListasEventos).build();
-	}
+        MaestroListasEventos.add(new EventsList(nombre));
+        // System.out.println("Se creó el evento " + listaEventos.get(0).getFecha());
+        return Response.status(201).entity(MaestroListasEventos).build();
+    }
 
-	/**
-	 * agrega un evento a una lista existente
-	 * 
-	 * @param nombreLista
-	 * @param nombreEvento
-	 * @return
-	 */
-	@Path("/agregarEventosLista/{nombreLista}/{nombreEvento}")
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response agregarEventosLista(@PathParam("nombreLista") String nombreLista,
-			@PathParam("nombreEvento") String nombreEvento) {
-		List<EventsList> coleccionListas = this.buscarListaEventosPorNombre(nombreLista);
+    /**
+     * agrega un evento a una lista existente
+     *
+     * @param nombreLista
+     * @param nombreEvento
+     * @return
+     */
+    @Path("/agregarEventosLista/{nombreLista}/{nombreEvento}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response agregarEventosLista(@PathParam("nombreLista") String nombreLista, @PathParam("nombreEvento") String nombreEvento) {
+        List<EventsList> coleccionListas = this.buscarListaEventosPorNombre(nombreLista);
 
-		for (EventsList lista : coleccionListas) {
-			lista.agregarEvento(this.buscarEventosPorNombre(nombreEvento));
-		}
+        for (EventsList lista : coleccionListas) {
+            lista.agregarEvento(this.buscarEventosPorNombre(nombreEvento));
+        }
 
-		// System.out.println("Se creó el evento " + evento);
-		return Response.status(201).entity(MaestroListasEventos).build();
+        // System.out.println("Se creó el evento " + evento);
+        return Response.status(201).entity(MaestroListasEventos).build();
 
-	}
+    }
 
-	/*
+    /*
 	 * *************************** métodos auxiliares
 	 * ***********************************
-	 */
+     */
+    private List<Event> buscarEventosPorNombre(String nombre) {
+        List<Event> result = new ArrayList<Event>();
+        for (Event evento : MaestroEventos) {
+            if (evento.seLlama(nombre)) {
+                result.add(evento);
+            }
+        }
+        return result;
 
-	private List<Event> buscarEventosPorNombre(String nombre) {
-		List<Event> result = new ArrayList<Event>();
-		for (Event evento : MaestroEventos) {
-			if (evento.seLlama(nombre)) {
-				result.add(evento);
-			}
-		}
-		return result;
-
-		/*
+        /*
 		 * //no me funciona el filter List<Event> eventoBuscado =
 		 * MaestroEventos.stream().filter(evento -> evento.seLlama(nombre))
 		 * .collect(Collectors.<Event> toList()); //System.out.println(eventoBuscado);
 		 * return Response.status(201).entity(eventoBuscado).build();
-		 */
-	}
+         */
+    }
 
-	/**
-	 * busca una lista de eventos por nombre
-	 * 
-	 * @param nombre
-	 * @return
-	 */
-	private List<EventsList> buscarListaEventosPorNombre(String nombre) {
-		List<EventsList> result = new ArrayList<EventsList>();
-		for (EventsList lista : MaestroListasEventos) {
-			if (lista.seLlama(nombre)) {
-				result.add(lista);
-			}
-		}
-		/*
+    /**
+     * busca una lista de eventos por nombre
+     *
+     * @param nombre
+     * @return
+     */
+    private List<EventsList> buscarListaEventosPorNombre(String nombre) {
+        List<EventsList> result = new ArrayList<EventsList>();
+        for (EventsList lista : MaestroListasEventos) {
+            if (lista.seLlama(nombre)) {
+                result.add(lista);
+            }
+        }
+        /*
 		 * MaestroListasEventos.forEach(lista -> { if (lista.seLlama(nombre)) {
 		 * //result.add(lista); } });
-		 */
-		return result;
-	}
+         */
+        return result;
+    }
 }
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -1,72 +1,95 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type"
-	content="text/html; charset=windows-1252">
-<title>JSP Page</title>
-<link href="/css/styles.css" rel="stylesheet">
-<style type="text/css">
-.contEvento {
-	float: left;
-	clear: both;
-}
+    <head>
+        <%@ page contentType="text/html; charset=iso-8859-1" language="java"  %>
+        <!--<meta charset="utf-8">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--<link rel="icon" href="../../../../favicon.ico">-->
+        <title>Administrador de Eventos TACS</title>
+        <!-- Bootstrap core CSS -->
+        <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/styles.css" rel="stylesheet">
+        <link href="/css/stickyFooter.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 3.5rem;
+            }
+        </style>
+    </head>
+    <%--         <h1>${it.hello} ${it.world}</h1> --%>
+    <%
+        String ServerName = request.getServerName();
+        String URL = "http://" + ServerName;
+        if (ServerName == null || ServerName.equals("localhost")) {
+            URL += ":" + request.getLocalPort();
+        }
+    %>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#">Inicio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-form{
-	width: 500px;
-}
-label,input,button {
-	float: left;
-	margin-bottom: 10px;
-}
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownEventos" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Eventos</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownEventos">
+                            <a class="dropdown-item" href="<%=URL%>/events">Eventos Disponibles</a>
+                            <a class="dropdown-item" href="#">Buscar Evento</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<%=URL%>/users">Usuarios</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownTelegram" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Telegram</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownTelegram">
+                            <a class="dropdown-item" href="<%=URL%>/telegram">Página Principal</a>
 
-label,button {
-	clear: left;
-}
-</style>
-</head>
-<body>
-	<%--         <h1>${it.hello} ${it.world}</h1> --%>
-	<%@ page contentType="text/html; charset=iso-8859-1" language="java"  %>
-	<%
- 		String ServerName=request.getServerName();
-		String URL = "";
-		if( ServerName != null && !ServerName.equals("localhost") ) {
-	       URL = ServerName;
-	   	}
-		else
-		{
-			URL = ServerName + ":" + request.getLocalPort();
-		}
-	%>
-		
-	<h1>Administrador de eventos</h1>
-	<div class="contEvento">
-		<h3>Telegram</h3>
-		<a href="http://<%=URL%>/telegram" target="_blank">http://<%=URL%>/telegram</a>
-	</div>
-	<div class="contEvento">
-		<h3>Ver usuarios</h3>
-		<a href="http://<%=URL%>/users" target="_blank">http://<%=URL%>/users</a>
-	</div>
-	<div class="contEvento">
-		<h3>Ver eventos disponibles</h3>
-		<a href="http://<%=URL%>/events" target="_blank">http://<%=URL%>/events</a>
-		<br> <br>
-	</div>
-	<div class="contEvento">
-		<h3>Buscar evento</h3>
-		<form method="post" action="http://<%=URL%>/events/buscarEvento">
-			<label>Código del evento</label> 
-			<input type="number" name="eventId" onkeypress="return event.charCode >= 48 && event.charCode <= 57" required/>
-			<button type="submit">Buscar</button>
-		</form>
-		<form method="post" action="http://<%=URL%>/events/buscarEvento">
- 			<label>Nombre del evento</label> <input type="text"	name="eventNombre" required/>
-			<button type="submit">Buscar</button>
-		</form>
-	</div>
-
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <main role="main">
+            <div class="jumbotron">
+                <div class="container">
+                    <h1 class="display-3 text-center">Administrador de eventos</h1>
+                    <!--<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>-->
+                    <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>-->
+                </div>
+            </div>
+        </main>
 
 
-</body>
+
+
+
+
+
+
+
+
+
+      
+
+
+        <!-- footer begin -->
+        <footer class="footer">
+            <div class="container">
+                <p class="text-muted">Sistema de administración de eventos - &copy; Grupo 5 TACS UTNBA</p>
+            </div>
+        </footer>
+        <!-- footer close -->
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/js/jquery-3.3.1.slim.min.js"></script>
+        <script src="/js/bootstrap/bootstrap.min.js"></script>
+    </body>
 </html>
